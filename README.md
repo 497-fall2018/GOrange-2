77b370109e8c9a42362ad721722b3a137e3c1944
Mark Jung <gujung2022@u.northwestern.edu>
2018-10-17 23:22:25 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is the server for EECS 497 project Court N run
+It's in go :)
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
+
+func Routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON), // sets content type to json
+		middleware.Logger, // log api calls
+		middleware.DefaultCompress, // compress results(mostly assets and json)
+		middleware.RedirectSlashes, // redirect slashes to no slash url versions (basic parsing?)
+		middleware.Recoverer, // recover from panic without crashing server
+	)
+	// version apis so you can update without breaking i
+	router.Route("/v1", func(r chi.Router) {
+		r.Mount("/api/free", free.Routes())
+	})
+
+	return router
+}
+
+func main() {
+	router := Routes()
+
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route) // Walk and print out all routes
+		return nil
+	}
+	if err := chi.Walk(router, walkFunc); err != nil {
+		log.Panicf("Logging err: %s\n", err.Error()) // panic if there is an error
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", router)) // Note, the port is usually gotten from the environment.
+
+}
