879c2c021c65f0ca0d0de65c2c3604477088c77d
Mark Jung <gujung2022@u.northwestern.edu>
2018-10-18 21:42:09 -0500

    with closures

--- features/gym/gym.go
+++ features/gym/gym.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
+	"github.com/497-fall2018/GOrange-2/internal/config"
 )
 
 type Gym struct {
@@ -13,45 +14,54 @@ type Gym struct {
 	Body  string `json:"body"`
 }
 
-func Routes() *chi.Mux {
+func Routes(configuration *config.Config) *chi.Mux {
 	router := chi.NewRouter()
-	router.Get("/{gymID}", GetGym)
-	router.Delete("/{gymID}", DeleteGym)
-	router.Post("/", CreateGym)
-	router.Get("/", GetAllGyms)
+	router.Get("/{gymID}", GetGym(configuration))
+	router.Delete("/{gymID}", DeleteGym(configuration))
+	router.Post("/", CreateGym(configuration))
+	router.Get("/", GetAllGyms(configuration))
 	return router
 }
 
-func GetGym(w http.ResponseWriter, r *http.Request) {
-	gymID := chi.URLParam(r, "gymID")
-	gyms := Gym{
-		Open:  false,
-		Title: "Hello world",
-		Body:  gymID,
-	}
-	render.JSON(w, r, gyms) // A chi router helper for serializing and returning json
-}
-
-func DeleteGym(w http.ResponseWriter, r *http.Request) {
-	gymID := chi.URLParam(r, "gymID")
-	response := make(map[string]string)
-	response["message"] = "Deleted Gym " + gymID
-	render.JSON(w, r, response) // Return some demo response
-}
-
-func CreateGym(w http.ResponseWriter, r *http.Request) {
-	response := make(map[string]string)
-	response["message"] = "Created Gym successfully"
-	render.JSON(w, r, response) // Return some demo response
-}
-
-func GetAllGyms(w http.ResponseWriter, r *http.Request) {
-	gyms := []Gym{
-		{
+func GetGym(configuration *config.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		gymID := chi.URLParam(r, "gymID")
+		gyms := Gym{
 			Open:  false,
 			Title: "Hello world",
-			Body:  "Heloo world from planet earth",
-		},
+			Body:  gymID,
+		}
+		render.JSON(w, r, gyms) // A chi router helper for serializing and returning json
 	}
-	render.JSON(w, r, gyms) // A chi router helper for serializing and returning json
+}
+
+func DeleteGym(configuration *config.Config) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		gymID := chi.URLParam(r, "gymID")
+		response := make(map[string]string)
+		response["message"] = "Deleted Gym " + gymID
+		render.JSON(w, r, response) // Return some demo response
+	}
+}
+
+
+func CreateGym(configuration *config.Config) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		response := make(map[string]string)
+		response["message"] = "Created Gym successfully"
+		render.JSON(w, r, response) // Return some demo response
+	}
+}
+
+func GetAllGyms(configuration *config.Config) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		gyms := []Gym{
+			{
+				Open:  false,
+				Title: "Hello world",
+				Body:  "Heloo world from planet earth",
+			},
+		}
+		render.JSON(w, r, gyms) // A chi router helper for serializing and returning json
+	}
 }
--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
 	"github.com/497-fall2018/GOrange-2/features/gym"
+	"github.com/497-fall2018/GOrange-2/internal/config"
 )
 
-func Routes() *chi.Mux {
+func Routes(configuration *config.Config) *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(
 		render.SetContentType(render.ContentTypeJSON), // sets content type to json
@@ -21,13 +22,17 @@ func Routes() *chi.Mux {
 	)
 	// version apis so you can update without breaking i
 	router.Route("/v1", func(r chi.Router) {
-		r.Mount("/api/gym", gym.Routes())
+		r.Mount("/api/gym", gym.Routes(configuration))
 	})
 
 	return router
 }
 
 func main() {
+	configuration, err := config.New()
+	if err != nil {
+		log.Panicln("Configuration error", err)
+	}
 	router := Routes()
 
 	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
@@ -38,6 +43,7 @@ func main() {
 		log.Panicf("Logging err: %s\n", err.Error()) // panic if there is an error
 	}
 
+	log.Println("Serving application at PORT :" + configuration.Constants.PORT)
 	log.Fatal(http.ListenAndServe(":8080", router)) // Note, the port is usually gotten from the environment.
 
 }
